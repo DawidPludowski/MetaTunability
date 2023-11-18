2f1d840d1571a2a23f7b5a58c29fb7e4b9713cde
DawidPludowski <dawid.pludowski@gmail.com>
2023-11-18 11:15:31 +0100

    add ConditionalGrid; remove size param

--- meta_tuner/searchers/search_grid.py
+++ meta_tuner/searchers/search_grid.py
@@ -39,27 +39,19 @@ class CubeGrid:
                 self.rngs.append(lambda: values)
             if isinstance(values, list):
                 if distribution == "uniform":
-                    self.rngs.append(
-                        lambda size: rng.uniform(values[0], values[1], size=size)
-                    )
+                    self.rngs.append(lambda: rng.uniform(values[0], values[1]))
                 elif distribution == "loguniform":
-                    self.rngs.append(
-                        lambda size: self.__lognuniform(values[0], values[1], size=size)
-                    )
+                    self.rngs.append(lambda: self.__lognuniform(values[0], values[1]))
         elif space == "int":
             if isinstance(values, int):
-                self.rngs.append(lambda size: np.repeat([values], repeats=size))
+                self.rngs.append(lambda: np.repeat([values]))
             if isinstance(values, list):
-                self.rngs.append(
-                    lambda size: rng.integers(values[0], values[1], size=size)
-                )
+                self.rngs.append(lambda: rng.integers(values[0], values[1]))
         elif space == "cat":
             if isinstance(values, str):
-                self.rngs.append(lambda size: np.repeat([values], repeats=size))
+                self.rngs.append(lambda: np.repeat([values]))
             if isinstance(values, (list, tuple)):
-                self.rngs.append(
-                    lambda size: rng.choice(values, size=size, replace=True)
-                )
+                self.rngs.append(lambda: rng.choice(values, replace=True))
         else:
             raise ValueError(
                 f'For arg "values" only "real", "int", "cat" are allowed. {values} provided'
@@ -70,7 +62,7 @@ class CubeGrid:
 
         self.names.append(name)
 
-    def pick(self, size: int = 1) -> Dict[str, np.ndarray[any]]:
+    def pick(self) -> Dict[str, np.ndarray[any]]:
         """
         Generate random point(s) from the grid.
         Args:
@@ -79,19 +71,43 @@ class CubeGrid:
         Returns:
             Dict[str, np.ndarray[any]]: List of points from grid.
         """
-        random_coordinates = [rng(size=size) for rng in self.rngs]
+        random_coordinates = [rng() for rng in self.rngs]
         dict_coordinates = {
             name: coordinates
             for name, coordinates in zip(self.names, random_coordinates)
         }
         return dict_coordinates
 
-    def __lognuniform(self, low=0, high=1, size=None, base=np.e):
+    def __lognuniform(self, low=0, high=1, base=np.e):
         rng = np.random.default_rng(seed=self.init_seed)
         rng_ = partial(rng.uniform, **{"low": low, "high": high})
 
-        return np.power(base, rng_(size=size)) / base
+        return np.power(base, rng_()) / base
 
 
 class ConditionalGrid:
-    pass
+    def __init__(self, init_seed: int = None) -> None:
+        self.init_seed = init_seed
+
+        self.cubes: List[CubeGrid] = []
+        self.conditions: List[Callable[..., bool]] = []
+
+    def add_cube(
+        self, cube: CubeGrid, condition: Callable[..., bool] = lambda pick: True
+    ) -> None:
+        self.cubes.append(cube)
+        self.conditions.append(condition)
+
+    def pick(self) -> Dict[str, np.ndarray[any]]:
+        pick = {}
+
+        for cube, condition in zip(self.cubes, self.conditions):
+            try:
+                if condition(pick):
+                    pick |= cube.pick()
+            except KeyError as e:
+                raise KeyError(
+                    f'In condition, key "{e.args[0]}" was used but not present in previous cubes.'
+                )
+
+        return pick
